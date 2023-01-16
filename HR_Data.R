4ed7b46774783c9e90899bc7a9d88868e9b729cf
CaptainB77 <beesquinca@gmail.com>
2023-01-16 16:36:59 -0600

    library and data

---
+++ HR_Data.R
@@ -0,0 +1,11 @@
+usethis::edit_git_config()
+usethis::use_git()
+use_github()
+usethis::use_github()
+
+library(tidyverse)
+library(readr)
+library (janitor)
+library(stringr)
+
+janitor::remove_empty()
