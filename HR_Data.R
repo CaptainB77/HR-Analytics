bb5d3e335568d6c0f8642f80d473c04443b12400
CaptainB77 <beesquinca@gmail.com>
2023-01-16 18:27:54 -0600

    Gender analyze

--- HR_Data.R
+++ HR_Data.R
@@ -7,7 +7,26 @@ library(tidyverse)
 library(readr)
 library (janitor)
 library(stringr)
+library(dplyr)
+library(ggplot2)
 
 summary(HR_Employee_Attrition)
 
+#Time to clean the data
 HrData = HR_Employee_Attrition [,c(-9,-22,-27)]
+
+#Let's take a look the data with some graphics
+#First let's see a little bit about the demographics of our dataset
+
+#Gender
+
+Gender2 = HrData %>% 
+  group_by(Gender) %>% 
+  count(Gender)
+
+ggplot(Gender2, aes(x = "", y = n, fill = Gender)) +
+  geom_col(color = "black") +
+  coord_polar(theta = "y") +
+  geom_text(aes(label = n),
+            position = position_stack(vjust = 0.5)) +
+  coord_polar(theta = "y")
